88a262604829f76fbb50ea76dabfb30919d35360
Krzysztof Misztal <krzysztof.misztal@gmail.com>
2017-03-24 13:43:05 +0100

    image backend

--- jist-api/src/main/java/pl/edu/misztal/JImageStreamToolkit/image/Image.java
+++ jist-api/src/main/java/pl/edu/misztal/JImageStreamToolkit/image/Image.java
@@ -1,5 +1,6 @@
 package pl.edu.misztal.JImageStreamToolkit.image;
 
+import pl.edu.uj.JImageStream.model.Pixel;
 import pl.edu.uj.JImageStream.model.StreamableImage;
 
 import java.awt.*;
@@ -8,8 +9,10 @@ import java.io.File;
 import java.io.IOException;
 
 public class Image extends StreamableImage implements Cloneable {
+    private UnpackedImage unpackedImage;
     public Image(File file) throws IOException {
         super(file);
+        this.unpackedImage = new UnpackedImage(this.bufferedImage);
     }
 
     public Image(String filename) throws IOException {
@@ -20,7 +23,7 @@ public class Image extends StreamableImage implements Cloneable {
         super(bufferedImage);
     }
 
-    private static BufferedImage copyImage(BufferedImage source) {
+    private static BufferedImage copyBufferedImage(BufferedImage source) {
         BufferedImage b = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
         Graphics g = b.getGraphics();
         g.drawImage(source, 0, 0, null);
@@ -41,7 +44,7 @@ public class Image extends StreamableImage implements Cloneable {
         Image ret = null;
         try {
             ret = (Image) super.clone();
-            ret.setBufferedImage(copyImage(bufferedImage));
+            ret.setBufferedImage(copyBufferedImage(bufferedImage));
         } catch (CloneNotSupportedException ignored) {
         }
         return ret;
@@ -54,4 +57,25 @@ public class Image extends StreamableImage implements Cloneable {
     public int getHeight() {
         return this.bufferedImage.getHeight();
     }
+
+    public final Pixel getPixel(int x, int y) {
+        return unpackedImage.getPixel(x, y);
+    }
+
+    public final int getRed(int x, int y) {
+        return unpackedImage.getPixel(x, y).getRed();
+    }
+
+    public final int getGreen(int x, int y) {
+        return unpackedImage.getPixel(x, y).getGreen();
+    }
+
+    public final int getBlue(int x, int y) {
+        return unpackedImage.getPixel(x, y).getBlue();
+    }
+
+    public final void setPixel(int x, int y, Pixel pixel) {
+        unpackedImage.setPixel(x, y, pixel);
+    }
+
 }
---
+++ jist-api/src/main/java/pl/edu/misztal/JImageStreamToolkit/image/UnpackedImage.java
@@ -0,0 +1,21 @@
+package pl.edu.misztal.JImageStreamToolkit.image;
+
+import pl.edu.uj.JImageStream.model.Pixel;
+
+import java.awt.image.BufferedImage;
+
+class UnpackedImage {
+    private pl.edu.uj.JImageStream.model.UnpackedImage unpackedImage;
+
+    UnpackedImage(BufferedImage bufferedImage) {
+        this.unpackedImage = new pl.edu.uj.JImageStream.model.UnpackedImage(bufferedImage);
+    }
+
+    void setPixel(int x, int y, Pixel pixel) {
+        this.unpackedImage.setPixel(x, y, pixel.getAlpha(), pixel.getRed(), pixel.getGreen(), pixel.getBlue();
+    }
+
+    Pixel getPixel(int x, int y) {
+        return new Pixel(this.unpackedImage.getPixel(x, y));
+    }
+}
