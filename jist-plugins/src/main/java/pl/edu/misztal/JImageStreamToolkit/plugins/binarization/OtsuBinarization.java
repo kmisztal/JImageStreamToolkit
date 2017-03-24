749eb2185178ffa38522f1114e2ba4b343ae561d
Krzysztof Misztal <krzysztof.misztal@gmail.com>
2017-03-24 12:27:50 +0100

    weighted otsu

--- jist-plugins/src/main/java/pl/edu/misztal/JImageStreamToolkit/plugins/binarization/OtsuBinarization.java
+++ jist-plugins/src/main/java/pl/edu/misztal/JImageStreamToolkit/plugins/binarization/OtsuBinarization.java
@@ -5,10 +5,19 @@ import pl.edu.misztal.JImageStreamToolkit.plugin.Plugin;
 
 
 public class OtsuBinarization extends Plugin {
+    private boolean weighted = false;
+
+    public OtsuBinarization() {
+    }
+
+    public OtsuBinarization(boolean weighted) {
+        this.weighted = weighted;
+    }
+
     public void process(Image imgIn, Image imgOut) {
         imgOut.setBufferedImage(
                 imgIn.parallelStream()
-                        .apply(new pl.edu.uj.JImageStream.filters.color.OtsuBinarization())
+                        .apply(new pl.edu.uj.JImageStream.filters.color.OtsuBinarization(weighted))
                         .collect(new pl.edu.uj.JImageStream.collectors.BufferedImageCollector())
         );
     }
