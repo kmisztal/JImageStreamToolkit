6206fd43b5cac19fc2f85f9bf003ff4871447940
Krzysztof Misztal <krzysztof.misztal@gmail.com>
2017-03-24 12:53:45 +0100

    morphology: erosion & dilation

---
+++ jist-plugins/src/main/java/pl/edu/misztal/JImageStreamToolkit/plugins/morphology/Dilation.java
@@ -0,0 +1,27 @@
+package pl.edu.misztal.JImageStreamToolkit.plugins.morphology;
+
+import pl.edu.misztal.JImageStreamToolkit.image.Image;
+import pl.edu.misztal.JImageStreamToolkit.plugin.Plugin;
+
+/**
+ * Created by krzys on 24.03.2017.
+ */
+public class Dilation extends Plugin {
+    private final int kernelSize;
+
+    public Dilation(int kernelSize) {
+        this.kernelSize = kernelSize;
+    }
+
+    public Dilation() {
+        this.kernelSize = 3;
+    }
+
+    public void process(Image imgIn, Image imgOut) {
+        imgOut.setBufferedImage(
+                imgIn.parallelStream()
+                        .apply(new pl.edu.uj.JImageStream.filters.morphology.ErosionFilter(this.kernelSize))
+                        .collect(new pl.edu.uj.JImageStream.collectors.BufferedImageCollector())
+        );
+    }
+}
