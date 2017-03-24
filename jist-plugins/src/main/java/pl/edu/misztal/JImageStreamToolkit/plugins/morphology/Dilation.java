2eff865feafb542bcc0ae1b9b8d71ae75628b77e
Krzysztof Misztal <krzysztof.misztal@gmail.com>
2017-03-24 13:07:54 +0100

    morphology: erosion & dilation with different kernels

--- jist-plugins/src/main/java/pl/edu/misztal/JImageStreamToolkit/plugins/morphology/Dilation.java
+++ jist-plugins/src/main/java/pl/edu/misztal/JImageStreamToolkit/plugins/morphology/Dilation.java
@@ -2,22 +2,28 @@ package pl.edu.misztal.JImageStreamToolkit.plugins.morphology;
 
 import pl.edu.misztal.JImageStreamToolkit.image.Image;
 import pl.edu.misztal.JImageStreamToolkit.plugin.Plugin;
+import pl.edu.uj.JImageStream.filters.morphology.DilationFilter;
 
 public class Dilation extends Plugin {
-    private final int kernelSize;
+    private int kernelSize = 3;
+    private int kernelShape = DilationFilter.BALL_KERNEL;
+
+    public Dilation(int kernelSize, int kernelShape) {
+        this.kernelSize = kernelSize;
+        this.kernelShape = kernelShape;
+    }
 
     public Dilation(int kernelSize) {
         this.kernelSize = kernelSize;
     }
 
     public Dilation() {
-        this.kernelSize = 3;
     }
 
     public void process(Image imgIn, Image imgOut) {
         imgOut.setBufferedImage(
                 imgIn.parallelStream()
-                        .apply(new pl.edu.uj.JImageStream.filters.morphology.ErosionFilter(this.kernelSize))
+                        .apply(new DilationFilter(this.kernelSize, this.kernelShape))
                         .collect(new pl.edu.uj.JImageStream.collectors.BufferedImageCollector())
         );
     }
--- jist-plugins/src/main/java/pl/edu/misztal/JImageStreamToolkit/plugins/morphology/Erosion.java
+++ jist-plugins/src/main/java/pl/edu/misztal/JImageStreamToolkit/plugins/morphology/Erosion.java
@@ -2,22 +2,28 @@ package pl.edu.misztal.JImageStreamToolkit.plugins.morphology;
 
 import pl.edu.misztal.JImageStreamToolkit.image.Image;
 import pl.edu.misztal.JImageStreamToolkit.plugin.Plugin;
+import pl.edu.uj.JImageStream.filters.morphology.ErosionFilter;
 
 public class Erosion extends Plugin {
-    private final int kernelSize;
+    private int kernelSize = 3;
+    private int kernelShape = ErosionFilter.BALL_KERNEL;
+
+    public Erosion(int kernelSize, int kernelShape) {
+        this.kernelSize = kernelSize;
+        this.kernelShape = kernelShape;
+    }
 
     public Erosion(int kernelSize) {
         this.kernelSize = kernelSize;
     }
 
     public Erosion() {
-        this.kernelSize = 3;
     }
 
     public void process(Image imgIn, Image imgOut) {
         imgOut.setBufferedImage(
                 imgIn.parallelStream()
-                        .apply(new pl.edu.uj.JImageStream.filters.morphology.ErosionFilter(this.kernelSize))
+                        .apply(new ErosionFilter(this.kernelSize, this.kernelShape))
                         .collect(new pl.edu.uj.JImageStream.collectors.BufferedImageCollector())
         );
     }
