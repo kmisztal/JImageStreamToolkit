e6d66756a933d608d7f3bfc7b0700070887f2fbf
Krzysztof Misztal <krzysztof.misztal@gmail.com>
2017-03-28 15:35:59 +0200

    histogram equalization + gray scale

--- jist-plugins/src/main/java/pl/edu/misztal/JImageStreamToolkit/plugins/color/HistogramEqualization.java
+++ jist-plugins/src/main/java/pl/edu/misztal/JImageStreamToolkit/plugins/color/HistogramEqualization.java
@@ -3,11 +3,11 @@ package pl.edu.misztal.JImageStreamToolkit.plugins.color;
 import pl.edu.misztal.JImageStreamToolkit.image.Image;
 import pl.edu.misztal.JImageStreamToolkit.plugin.Plugin;
 
-public class GrayScale extends Plugin {
+public class HistogramEqualization extends Plugin {
     public void process(Image imgIn, Image imgOut) {
         imgOut.setBufferedImage(
                 imgIn.parallelStream()
-                        .apply(new pl.edu.uj.JImageStream.filters.color.GrayScaleFilter())
+                        .apply(new pl.edu.uj.JImageStream.filters.color.HistogramEqualization())
                         .collect(new pl.edu.uj.JImageStream.collectors.BufferedImageCollector())
         );
     }
